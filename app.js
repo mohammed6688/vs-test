b75729925e34492eab98d4d43b9aa038b297c486
mohammed6688 <mohammed668800.ma@gmail.com>
2022-05-20 09:50:14 +0200

    v2

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-var express = require('express');
-app = express();
+var express = require("express");
+const app = express();
+
+var response;
+
+app.get('/', function (req, res) {
+    response = 'This is version 2 of the app.' + '\n';
+    //send the response to the client
+    res.send(response);
+});
+
 app.get('/mars', function(req, res) {
     res.send('Hello Mars!\n');
   });
-  app.listen(8080,function(){
-      console.log("exaple app listener");
-  });
+
+app.listen(8080,function(){
+        console.log("exaple app listener");
+});
